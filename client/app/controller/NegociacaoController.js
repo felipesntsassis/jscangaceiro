0ab223aa52443a59914945297ad928e50c14e4cc
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2018-05-16 00:03:02 -0300

    Codificação realizada no capítulo 4.

--- client/app/controller/NegociacaoController.js
+++ client/app/controller/NegociacaoController.js
@@ -8,18 +8,15 @@ class NegociacaoController {
 
     adiciona(event) {
         event.preventDefault();
-        let data = new Date(...this._inputData.value
-            .split('-')
-            .map((item, indice) => item - indice % 2)
-        );
-        console.log(data);
 
-        let negociacao = new Negociacao(data, 
+        let negociacao = new Negociacao(
+            DateConverter.paraData(this._inputData.value), 
             parseInt(this._inputQuantidade.value), 
             parseFloat(this._inputValor.value)
         );
+        console.log(negociacao.data);
 
-        console.log(negociacao);
-        
+        let diaMesAno = DateConverter.paraTexto(negociacao.data);
+        console.log(diaMesAno);
     }
 }
