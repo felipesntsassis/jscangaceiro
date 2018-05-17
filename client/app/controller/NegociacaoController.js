e09dd062fc2470fb53f535bd95f3f74b8b7771d8
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2018-05-17 08:16:49 -0300

    Códigos estudados no capítulo 6.

--- client/app/controller/NegociacaoController.js
+++ client/app/controller/NegociacaoController.js
@@ -4,19 +4,30 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._negociacoes = new Negociacoes();
+        this._negociacoesView = new NegociacoesView('#negociacoes');
+        this._negociacoesView.update(this._negociacoes);
     }
 
     adiciona(event) {
         event.preventDefault();
+        this._negociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._negociacoes);
+        this._limparFormulario();
+    }
 
-        let negociacao = new Negociacao(
-            DateConverter.paraData(this._inputData.value), 
-            parseInt(this._inputQuantidade.value), 
+    _limparFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
+    }
+
+    _criaNegociacao() {
+        return new Negociacao(
+            DateConverter.paraData(this._inputData.value),
+            parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
-        console.log(negociacao.data);
-
-        let diaMesAno = DateConverter.paraTexto(negociacao.data);
-        console.log(diaMesAno);
     }
 }
