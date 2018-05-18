d3da38c751f0665e7563922630d38f8d4d4649a7
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2018-05-18 08:37:33 -0300

    Códigos elaborados na aula 8.

--- client/app/controller/NegociacaoController.js
+++ client/app/controller/NegociacaoController.js
@@ -4,7 +4,10 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._negociacoes = new Negociacoes();
+        this._negociacoes = new Negociacoes(model => {
+            console.log(this);
+            this._negociacoesView.update(model);
+        });
         this._negociacoesView = new NegociacoesView('#negociacoes');
         this._negociacoesView.update(this._negociacoes);
         this._mensagem = new Mensagem();
@@ -16,9 +19,14 @@ class NegociacaoController {
         event.preventDefault();
         this._negociacoes.adiciona(this._criaNegociacao());
         this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._negociacoesView.update(this._negociacoes);
         this._mensagemView.update(this._mensagem);
         this._limparFormulario();
+    }
+
+    apaga() {
+        this._negociacoes.esvazia();
+        this._mensagem.texto = 'Negociações apagadas com sucesso';
+        this._mensagemView.update(this._mensagem);
     }
 
     _limparFormulario() {
--- client/app/domain/negociacao/Negociacoes.js
+++ client/app/domain/negociacao/Negociacoes.js
@@ -1,18 +1,26 @@
 class Negociacoes {
-    constructor() {
+    constructor(armadilha) {
         this._negociacoes = [];
+        this._armadilha = armadilha;
+        Object.freeze(this);
     }
 
     adiciona(negociacao) {
         this._negociacoes.push(negociacao);
+        this._armadilha(this);
+    }
+
+    esvazia() {
+        this._negociacoes.length = 0;
+        this._armadilha(this);
     }
 
     paraArray() {
         return [].concat(this._negociacoes);
     }
-
+    
     get volumeTotal() {
         return this._negociacoes.reduce((total, negociacao) =>
-            total + negociacao.volume, 0);
+        total + negociacao.volume, 0);
     }
 }
